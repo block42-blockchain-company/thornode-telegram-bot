4c9f81bd73f7a4eba0b298aad59082ea239d82b3
Paul to the Plessing <paul.plessing@block42.tech>
2020-04-06 19:28:21 +0700

    added recurring node check, but not working yet

--- thornode_alert.py
+++ thornode_alert.py
@@ -1,7 +1,8 @@
 import logging
 import os
-
+import random
 import requests
+import json
 
 from telegram.ext import (Updater, CommandHandler, PicklePersistence)
 
@@ -13,31 +14,123 @@ logger = logging.getLogger(__name__)
 
 telegram_token = os.environ["TELEGRAM_TOKEN"]
 
+node_fields = ["status", "bond", "slash_points"]
 
 def start(update, context):
     update.message.reply_text(
-        'Hi!. '
-        'Here you will get some nice jsons of THORnodes.\n\n'
-        'Look at this:')
+        'Heil ok sæll! 😁\n'
+        'This is the Telegram Alert Bot for THORnodes of THORchain.\n\n'
+        'Enter the address of a THORnode, and the Bot will notify you if there is a change in the nodes status, bond or slash_points.\n'
+        'Commands:\n'
+        '/enterThornodeAddress thorXXX..XXX\n'
+        '/getStats')
 
-    url = "http://35.170.66.96:1317/thorchain/nodeaccounts"
-    request = requests.get(url)
-    data = request.json()
+    #url = "http://67.205.166.241:1317/thorchain/nodeaccounts"
+    #request = requests.get(url)
+    #data = request.json()
+#
+    #update.message.reply_text(data[0])
 
-    update.message.reply_text(data[0])
 
-def add(update, context):
+def enterThornodeAddress(update, context):
     address = update.message.text.partition(' ')[2]
-    context.user_data["thornode_address"] = address
+    url = getRandNode() + ':1317/thorchain/nodeaccounts'
 
-    update.message.reply_text('The address you listen to is: ' + address)
+    request = requests.get(url)
+    allNodesJson = request.json()
 
-def get(update, context):
-    update.message.reply_text('The address you listen to is: ' + context.user_data["thornode_address"])
+    node = getThorNodeObject(allNodesJson, address)
+    if node is None:
+        return update.message.reply_text('⛔️ The THORnode address you entered does not exist! ⛔️\n'
+                                         '                      Please try again.')
+
+    context.user_data["address"] = address
+    for field in node_fields:
+        context.user_data[field] = node[field]
+
+    # context.user_data["status"] = node["status"]
+    # context.user_data["bond"] = node["bond"]
+    # context.user_data["slash_points"] = node["slash_points"]
+
+    text = 'The address you monitor is: ' + address
+
+    for field in node_fields:
+        text += '\nCurrent ' + field + ': ' + node[field]
+
+    update.message.reply_text(text)
+
+    # Add job to queue and stop current one if there is a timer already
+    if 'checkNodeJob' in context.user_data:
+        old_job = context.user_data['checkNodeJob']
+        old_job.schedule_removal()
+    new_job = context.job_queue.run_repeating(checkNode, 30, context=update)
+    context.user_data['checkNodeJob'] = new_job
+
+
+def getStats(update, context):
+    # update.message.reply_text('The address you listen to is: ' + context.user_data["thornode_address"])
+    update.message.reply_text('The address you listen to is: ' + context.user_data["address"] +
+                              '\nCurrent status: ' + context.user_data["status"] +
+                              '\nCurrent bond: ' + context.user_data["bond"] +
+                              '\nCurrent slash_points: ' + context.user_data["slash_points"])
+
 
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def getRandNode():
+    #url = "https://testnet-seed.thorchain.info/"
+    #request = requests.get(url)
+    #data = request.json()
+    #
+    #randNodeIndex = random.randrange(0, len(data.active))
+    #randNode = data.active[randNodeIndex]
+
+    hardcoded_node = "http://67.205.166.241"
+    return hardcoded_node
+
+
+def getThorNodeObject(allNodesJson, address):
+    for i in range(0, len(allNodesJson)):
+        if allNodesJson[i]["node_address"] == address:
+            return allNodesJson[i]
+    return None
+
+
+def checkNode(context):
+    job = context.job
+
+    address = context.user_data['address']
+    url = getRandNode() + ':1317/thorchain/nodeaccounts'
+
+    request = requests.get(url)
+    allNodesJson = request.json()
+
+    node = getThorNodeObject(allNodesJson, address)
+    if node is None:
+        job.context.message.reply_text('⛔️ The THORnode you monitor is not active anymore! ⛔️\n'
+                                       '                Please set a new THORnode address.')
+        old_job = context.user_data['checkNodeJob']
+        old_job.schedule_removal()
+        return
+
+    for field in node_fields:
+        if context.user_data[field] != node[field]:
+            text = field + ' changed! 💥\n THORnode address: ' + address
+            for key in node_fields:
+                text += '\n' + key + ': ' + context.user_data[key] + ' ➡️ ' + node[key]
+                context.user_data[key] = node[key]
+
+            job.context.message.reply_text(text)
+            return
+
+    #context.user_data["status"] = node["status"]
+    #context.user_data["bond"] = node["bond"]
+    #context.user_data["slash_points"] = node["slash_points"]
+
+    #job.context.message.reply_text("hi")
 
 
 def main():
@@ -53,10 +146,12 @@ def main():
 
     # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
 
-
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("add", add))
-    dp.add_handler(CommandHandler("get", get))
+    dp.add_handler(CommandHandler("enterThornodeAddress", enterThornodeAddress,
+                                  pass_args=True,
+                                  pass_job_queue=True,
+                                  pass_chat_data=True))
+    dp.add_handler(CommandHandler("getStats", getStats))
 
     # log all errors
     dp.add_error_handler(error)
