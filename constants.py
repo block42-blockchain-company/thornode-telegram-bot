05a99bb9d6c46e95b274dc1d5919760565422f45
Paul to the Plessing <paul.plessing@block42.tech>
2020-04-09 16:35:31 +0700

    constants file

---
+++ constants.py
@@ -0,0 +1,10 @@
+import os
+
+TYPING_ADDRESS = range(1)
+
+TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
+
+NODE_FIELDS = ["status", "bond", "slash_points"]
+
+HARDCODED_NODE = "http://67.205.166.241:1317/thorchain/nodeaccounts"
+HARDCODED_LOCAL_NODE = "http://localhost:8000/node_data.json"
