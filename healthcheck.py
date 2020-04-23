262ebaa53b0fa023afc8459b2a5c761d15425c10
Paul to the Plessing <paul.plessing@block42.tech>
2020-04-23 17:24:58 +0700

    resolved pull request comments

--- healthcheck.py
+++ healthcheck.py
@@ -5,12 +5,9 @@ UNHEALTHY = 1
 
 
 def check_health():
-    return UNHEALTHY
-    timestamp_benchmark = datetime.timestamp(datetime.now() - timedelta(seconds=30))
-    with open('storage/health.check') as healthcheck_file:
-        thornode_bot_timestamp = healthcheck_file.read()
+    last_thornode_bot_timestamp = open('storage/health.check').read()
 
-    if float(thornode_bot_timestamp) > timestamp_benchmark:
+    if float(last_thornode_bot_timestamp) >= float(datetime.timestamp(datetime.now() - timedelta(seconds=30))):
         return HEALTHY
     else:
         return UNHEALTHY
