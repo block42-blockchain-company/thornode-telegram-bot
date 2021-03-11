c3fcfcc95c296edc4d4ba161cd38b7bee4efbd8c
Oliver Schwarz <46133123+DollyVolley@users.noreply.github.com>
2021-03-11 17:14:50 +0100

    Fix/testnet profit rollup (#149)
    * Hotfix add missing requirement
    
    * Separate mongodb to testnet and chaosnet instance
    
    * Configure tendermint client for testnet, Remove chaosnet snapshot from testnet setup
    
    * Circumvent ciruclar import
    
    * Distinguish between testnet and chaosnet tendermint client

--- bot/constants/globals.py
+++ bot/constants/globals.py
@@ -67,4 +67,9 @@ SECONDS_PER_BLOCK = 5.5
 RUNE_DECIMALS = 100000000
 
 # MongoDB Settings
-MONGO_URL = "localhost:27017/" if NATIVE_DEPLOYMENT else "thornode-bot-mongodb:27017/"
+NETWORK_TYPES = ["TESTNET", "CHAOSNET"]
+NETWORK_TYPE = os.getenv("NETWORK_TYPE").upper() \
+    if os.getenv("NETWORK_TYPE", "notFound").upper() in NETWORK_TYPES and not DEBUG else 'TESTNET'
+
+PROD_MONGO_URL = "thornode-bot-chaosnet-mongodb:27017/" if NETWORK_TYPE == "CHAOSNET" else "thornode-bot-testnet-mongodb:27017/"
+MONGO_URL = "localhost:27017/" if NATIVE_DEPLOYMENT else PROD_MONGO_URL
